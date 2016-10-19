57c46c8dc72e4bd0af366521b309b46734e6ee92
zimsteve <zimsteve@live.com>
2016-10-19 15:23:59 +1300

    working for cases except 3 ball 10th frame

---
+++ main.js
@@ -0,0 +1,29 @@
+var frames = [
+    [1, 2], [6, 4], [5, 4], [10, 0], [7, 2], [10, 0], [10, 0], [5, 2], [7, 0], [4, 4] //total = 119
+  ]
+
+function sumScore (game) {
+  var total = 0;
+
+  for (var i = 0; i < 10; i++) {
+     var sum = game[i][0] + game[i][1];
+     if (sum === 10) { //if a strike or a spare
+       sum += game[i + 1][0]; //add ball 1, 1 frame over
+        if (game[i][0] === 10) { //if a strike
+          if (sum === 20) { //if follow up is a strike
+            sum += game[i+2][0]; //ad ball 1 two frames over
+          } else {
+            sum += game[i+1][1]; //add ball 2 1 frame over
+          }
+        }
+     }
+     console.log(total + " + " + sum);
+       total += sum;
+     }
+     return total;
+   }
+
+console.log(sumScore(frames));
+// function main() {
+//   return sumScore(frames);
+// }
